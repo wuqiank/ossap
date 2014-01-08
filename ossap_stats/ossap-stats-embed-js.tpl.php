f6333fb7d893a2431f5244e01f212e8b35608a6a
oren <get.oren@gmail.com>
2014-01-08 16:22:41 -0500

    Unify all embed stats into 1 file #8

--- ossap_stats/ossap-stats-embed-js.tpl.php
+++ ossap_stats/ossap-stats-embed-js.tpl.php
@@ -3,19 +3,53 @@
 /**
  * @file
  * Javascript source to print the total sites onto the page.
+ *
+ * Available variables:
+ *
+ * $aggregates - the value of the 'ossap_stats_aggregates' variable, an indexed
+ * array where keys are the statistic's description, and values are numeric
+ * values.
  */
 
-if(empty($stats)){
-  $stats = array();
-}
+global $base_url;
+$src = "$base_url/ossap/stats.js";
 ?>
-
-
-/** Updates the div element to contain the latest total number of vsites. */
-function ossapStatsGet(stat) {
+/**
+ * Include this JS file to embed aggregated OpenScholar SAP stats on any page.
+ * Like this:
+ * @code
+ * <script type="text/javascript" src="<?php echo $src; ?>"></script>
+ * @endcode
+ *
 <?php
-foreach (variable_get('os_stats_enabled',array('websites')) as $stat){
-  echo "if (stat == '{$stat}') {\nreturn '{$stats[$stat]}'; ?>';\n}\n";
+if (empty($aggregates)) {
+  echo " * Something is wrong. Either no child domains are configured, or the\n";
+  echo " * ossap_stats cron job needs to be run. Contact a system administrator.\n";
+  echo " */";
+  die();
 }
 ?>
-}
+ * Your HTML markup may use any of the following id attributes to get numbers:
+ * @code
+<? foreach ($aggregates as $key => $value): ?>
+ * <div id="ossap-stats-<?php echo $key; ?>"></div>
+<? endforeach; ?>
+ * @endcode
+ *
+ * Note: The examples above use DIV tags, but you may use SPAN, P, etc.
+ *
+ * @see https://github.com/openscholar/ossap
+ */
+(function(){
+
+<? foreach ($aggregates as $key => $value): ?>
+  // Total <?php echo $key; ?>
+
+  var elem = document.getElementById('ossap-stats-<?php echo $key; ?>');
+  if (typeof div !== 'undefined' && div !== null) {
+    <?php $value = ($key == 'filesize_bytes') ? format_size($value) : $value ?>
+    div.innerText = '<?php echo $value; ?>';
+  }
+
+<? endforeach; ?>
+})();
